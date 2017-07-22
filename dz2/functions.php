0b729cd2b2a576f742236c129399b7e7a7377b7f
Vladimir Tarakanovskij <tarvl@bk.ru>
2017-07-22 16:45:42 +0300

    221645

--- dz2/functions.php
+++ dz2/functions.php
@@ -1,32 +1,12 @@
 <?php 
-function task1(){
-	
-}
-function task2(){
-	
-}
-function task3(){
-	
-}
-function task4(){
-	
-}
-function task5(){
-	
-}
-function task6(){
-	
-}
-function task7(){
-	
-}
-function task8(){
-	
-}
-function task9(){
-	
-}
-function task10(){
-	
-}
+function task1(){}
+function task2(){}
+function task3(){}
+function task4(){}
+function task5(){}
+function task6(){}
+function task7(){}
+function task8(){}
+function task9(){}
+function task10(){}
 ?>
