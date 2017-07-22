2f187b532a20e5c1f1917bf3110a00f00a1cf865
Vladimir Tarakanovskij <tarvl@bk.ru>
2017-07-22 17:15:19 +0300

    221715

--- dz1/cp1-33.php
+++ dz1/cp1-33.php
@@ -2,11 +2,17 @@
 	
 	//развернутая строка
 	$str = 'Тестовая строка по первому домашнему заданию'; echo $str.'<br/>';
-	$str_arr = explode(" ", $str); '<p>'.print_r($str_arr).'</p>'; 
-	$c_str = count($str_arr);
-	while ($c_str >= 0) {
-		$rev_str_arr = $str_arr[$c_str-1]; $c_str--;}
-	$rev_str = implode(',', $rev_str_arr); echo '<p>'.$rev_str.'</p>';
+	$str_arr = explode(" ", $str); '<p>'.print_r($str_arr).'</p><br/>'; 
+	$c_str = count($str_arr); $i = 0;
+	while ($i < $c_str / 2) {
+	    $buf_str_arr = $str_arr[$i];
+	    $str_arr[$i] = $str_arr[$c_str - $i - 1];
+	    $str_arr[$c_str - $i - 1] = $buf_str_arr; $i++;
+	}
+	/*while ($c_str >= 0) {
+		$rev_str_arr = $str_arr[$c_str-1]; $c_str--;}*/
+	echo '<pre>' . var_dump($str_arr) . "</pre>\n";
+	$rev_str = implode(', ', $str_arr); echo '<p>'.$rev_str.'</p>';
 	
 	// таблица
 	$cols=10; $rows=10;
