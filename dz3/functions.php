84711f7268c099cc5bdb37ce9850fb43278f6777
Vladimir Tarakanovskij <tarvl@bk.ru>
2017-08-17 00:29:20 +0300

    170030

---
+++ dz3/functions.php
@@ -0,0 +1,14 @@
+<?php 
+function task1(){ 
+	
+}
+function task2(){
+	
+}
+function task3(){
+	
+}
+function task4(){
+	
+}
+?>
---
+++ dz3/index.php
@@ -0,0 +1,7 @@
+<?php 
+require 'functions.php';
+echo task1().'<br/>';
+echo task2().'<br/>';
+echo task3();
+task4();
+?>
