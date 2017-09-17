43fe62d9d9fb820aef7ffe41ff9dcd4d96fd7378
Vladimir Tarakanovskij <tarvl@bk.ru>
2017-09-18 00:50:14 +0300

    180050

---
+++ vp1/vp1.sql
@@ -0,0 +1,86 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Сен 18 2017 г., 00:44
+-- Версия сервера: 5.5.53
+-- Версия PHP: 7.0.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `vp1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(4) UNSIGNED NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(15) NOT NULL
+) ENGINE=MRG_MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users_detail`
+--
+
+CREATE TABLE `users_detail` (
+  `id` int(4) UNSIGNED NOT NULL,
+  `street` varchar(100) NOT NULL,
+  `home` varchar(10) NOT NULL,
+  `part` int(2) UNSIGNED NOT NULL,
+  `appt` int(3) UNSIGNED NOT NULL,
+  `floor` int(2) UNSIGNED NOT NULL,
+  `payment` varchar(50) NOT NULL,
+  `callback` tinyint(1) NOT NULL,
+  `comment` text NOT NULL
+) ENGINE=MRG_MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users_detail`
+--
+ALTER TABLE `users_detail`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(4) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `users_detail`
+--
+ALTER TABLE `users_detail`
+  MODIFY `id` int(4) UNSIGNED NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
